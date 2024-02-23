a09f253103b5c494e8b80066809a28dde957ad1e
julianintila <114980461+julianintila@users.noreply.github.com>
2024-02-23 14:31:40 +0800

    Add files via upload

---
+++ Prelims_Intila.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 20px 0;
+}
+
+.main-content {
+    text-align: center;
+    padding: 50px 0;
+}
+
+.btn {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #333;
+    color: #fff;
+    text-decoration: none;
+    border-radius: 5px;
+    transition: background-color 0.3s ease;
+}
+
+.btn:hover {
+    background-color: #555;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 20px 0;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+
+    </style>
+</head>
+<body>
+<header>
+        <h1>Welcome TO MY WEBSITEEE</h1>
+    </header>
+    <section class="main-content">
+        <p>WHAHAHAHAHAH.</p>
+        <a href="#" class="btn">Learn More</a>
+    </section>
+    <footer>
+        <p>&copy; 2024 Simple Landing Page</p>
+    </footer>
+</body>
+</html>
